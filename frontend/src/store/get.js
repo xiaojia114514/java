7271ff4987323caa92d76b1300e0888339315a44
xiaojia <12834440+small-leave@user.noreply.gitee.com>
2026-03-22 19:40:24 +0800

    1

--- frontend/src/store/get.js
+++
@@ -1,8 +0,0 @@
-const get={
-  token: state=>state.user.token,
-  permmited: state=>state.user.permitted,
-  UAccount: state=>state.user.user.UAccount,
-  permittedRoutes: state=>state.user.permittedroutes,
-  user:state=>state.user.user
-}
-export default get
